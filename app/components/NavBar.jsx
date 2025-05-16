73cd0bd9d9827e025754a1901d47e7003a883988
kamsi obiwulu <kamsiobiwulu@gmail.com>
2025-05-16 10:24:36 +0100

    first commit

---
+++ app/components/NavBar.jsx
@@ -0,0 +1,46 @@
+'use client'
+import Link from "next/link";
+import React from "react";
+import { IoSearchOutline } from "react-icons/io5";
+import { MdPersonOutline } from "react-icons/md";
+import { useAppContext } from "../context/AppContext";
+
+const Navbar = () => {
+  const isSeller = true;
+	const { router } = useAppContext()
+  return (
+	<div className="flex items-center justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-200 text-gray-900">
+	  <h1 onClick={() => router.push('/')} className="text-2xl font-medium">
+		<span className="text-orange-500">Q</span>uickCart
+	  </h1>
+	  <div className="flex items-center gap-4 lg:gap-8 max-md:hidden">
+		<Link href={"/"} className="hover:text-gray-700 transition">
+		  Home
+		</Link>
+		<Link href={"/"} className="hover:text-gray-700 transition">
+		  Shop
+		</Link>
+		<Link href={"/"} className="hover:text-gray-700 transition">
+		  About Us
+		</Link>
+		<Link href={"/"} className="hover:text-gray-700 transition">
+		  Contact
+		</Link>
+		{isSeller && (
+		  <button className="text-xs border px-4 py-2.5 rounded">
+			Seller Dashboard
+		  </button>
+		)}
+	  </div>
+	  <div className="flex items-center gap-4">
+		<IoSearchOutline />
+		<button className="flex items-center gap-2 hover:text-gray-900 transition">
+		  <MdPersonOutline />
+		  Account
+		</button>
+	  </div>
+	</div>
+  );
+};
+
+export default Navbar;
---
+++ app/layout.js
@@ -0,0 +1,34 @@
+import { Outfit } from "next/font/google";
+import "./globals.css";
+import { AppContextProvider } from "./context/AppContext";
+import { Toaster } from "react-hot-toast";
+
+const outfit = Outfit({
+  weights: ["300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "QuickCart",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+  description: "A modern e-commerce platform for quick shopping.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <AppContextProvider>
+      <Toaster />
+      <html lang="en" className={` ${outfit.className}`}>
+        <body
+          className={`${outfit.className} antialiased`}
+        >
+          {children}
+        </body>
+      </html>
+    </AppContextProvider>
+  );
+}
